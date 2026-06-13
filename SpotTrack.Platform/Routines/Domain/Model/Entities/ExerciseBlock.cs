9e9af16cab27d3fbdb387dcebf7aba24d62ddd84
ORION-tech-c <asfernandezl.peru@gmail.com>
2026-06-13 14:24:28 -0500

    feat(routines): new entity added

--- SpotTrack.Platform/Routines/Domain/Model/Entities/ExerciseBlock.cs
+++ SpotTrack.Platform/Routines/Domain/Model/Entities/ExerciseBlock.cs
@@ -1,6 +1,24 @@
+using SpotTrack.Platform.Routines.Domain.Model.ValueObjects;
+
 namespace SpotTrack.Platform.Routines.Domain.Model.Entities;
 
 public class ExerciseBlock
 {
+    public int Id { get; private set; }
     
+    public ExerciseName Name { get; private set; }
+    
+    public ExerciseType Type { get; private set; }
+    
+    public int Order { get; private set; }
+
+    public ExerciseBlock(int id, ExerciseName name, ExerciseType type, int order)
+    {
+        if (order <= 0)
+            throw new ArgumentException("Order cannot be zero or negative.", nameof(order));
+        Id = id;
+        Name = name;
+        Type = type;
+        Order = order;
+    }
 }
